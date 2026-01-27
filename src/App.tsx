91fc26069f9fa0ba33e8c577bd4739285f0bd1fc
Chernavtsev AV <chernavtsev.av@gmail.com>
2026-01-27 16:14:52 +1000

    add canvas size

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
 				<a href="https://react.dev" target="_blank">
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
+				<h1>Canvas</h1>
 			</div>
-			<h1>Canvas</h1>
 			<Canvas />
 		</>
 	)
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,51 +1,62 @@
-import { useEffect, useRef, useState } from "react";
-import { Stage, Layer, Rect, Text, Circle } from "react-konva";
-import Konva from "konva";
+import { useEffect, useRef, useState } from 'react'
+import { Stage, Layer, Rect, Text, Circle } from 'react-konva'
+import Konva from 'konva'
 
 export const ColoredRect = () => {
-  const [color, setColor] = useState("green");
+	const [color, setColor] = useState('green')
 
-  const handleClick = () => {
-    setColor(Konva.Util.getRandomColor());
-  };
+	const handleClick = () => {
+		setColor(Konva.Util.getRandomColor())
+	}
 
-  return (
-    <Rect
-      x={20}
-      y={20}
-      width={50}
-      height={50}
-      draggable
-      fill={color}
-      shadowBlur={5}
-      onClick={handleClick}
-      onDragEnd={(e) => {
-        console.log(e);
-        setColor(Konva.Util.getRandomColor());
-      }}
-    />
-  );
-};
+	return (
+		<Rect
+			x={20}
+			y={20}
+			width={50}
+			height={50}
+			draggable
+			fill={color}
+			onClick={handleClick}
+			onDragEnd={(e) => {
+				console.log(e)
+				setColor(Konva.Util.getRandomColor())
+			}}
+		/>
+	)
+}
 
 const MyShape = () => {
-  const circleRef = useRef(null);
+	const circleRef = useRef(null)
 
-  useEffect(() => {
-    // log Konva.Circle instance
-    console.log(circleRef.current);
-  }, []);
+	useEffect(() => {
+		// log Konva.Circle instance
+		console.log(circleRef.current)
+	}, [])
 
-  return <Circle ref={circleRef} radius={50} fill="black" />;
-};
+	return <Circle ref={circleRef} radius={50} fill="black" />
+}
 
 export const Canvas = () => {
-  return (
-    <Stage width={400} height={400}>
-      <Layer>
-        <Text x={60} text="Try click on rect" />
-        <ColoredRect />
-        <MyShape />
-      </Layer>
-    </Stage>
-  );
-};
+	const wrapperRef = useRef<HTMLDivElement>(null)
+	const [size, setSize] = useState({ width: 100, height: 100 })
+	useEffect(() => {
+		// eslint-disable-next-line react-hooks/set-state-in-effect
+		setSize({
+			width: wrapperRef.current?.clientWidth || 100,
+			height: wrapperRef.current?.clientHeight || 100,
+		})
+	}, [])
+
+	return (
+		<div ref={wrapperRef} className="canvas-wrapper">
+			<Stage draggable {...size}>
+				<Layer>
+					<Text x={60} text="Try click on rect" />
+					<ColoredRect />
+					<MyShape />
+				</Layer>
+			</Stage>
+		</div>
+	)
+}
